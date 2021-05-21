a9d6dd9047d9984d3011c68fb7da13366330067b
Roberto <roberto.cantu@gmail.com>
2021-05-21 01:06:32 -0500

    Include Parser

--- MemoryManagment/main.cpp
+++ MemoryManagment/main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
+#include <Parser.h>
+#include <string>
 
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+vector <vector <string>> instructions;
+
+
+Parser parser ("Test.txt");
+instructions= parser.getParseInstructions();
+//cout << instructions.size();
+
+
+
+
+
+for (int i=0; i < instructions.size();i++){
+    for (int j=0; j <instructions[i].size();j++){
+        cout << instructions[i][j];
+    }
+    cout << "\n";
+}
+
+
+
+
+
     return 0;
 }
