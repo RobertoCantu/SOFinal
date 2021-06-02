fbb20d9d5142a394c682313ed428ab3ce438b894
Roberto <roberto.cantu@gmail.com>
2021-06-01 20:59:52 -0500

    Add Instructions class

---
+++ MemoryManagment/include/Insturctions.h
@@ -0,0 +1,16 @@
+#ifndef INSTURCTIONS_H
+#define INSTURCTIONS_H
+
+
+class Insturctions
+{
+    public:
+        Insturctions();
+        virtual ~Insturctions();
+
+    protected:
+
+    private:
+};
+
+#endif // INSTURCTIONS_H
--- MemoryManagment/main.cpp
+++ MemoryManagment/main.cpp
@@ -13,26 +13,44 @@ int main()
 vector <vector <string>> instructions;
 
 
-Parser parser ("Test.txt");
+Parser parser ("ArchivoTrabajo.txt");
 
 instructions= parser.getParseInstructions();
 
 
 
+for (int i=0; i < instructions.size(); i++){
+
+    if (instructions[i][0] == "A"{
+        // Metodo para instuccion
+        })
+
+        else if (instructions[i][0] == "P"{
+        // Metodo para instuccion
+        })
+
+        else if (instructions[i][0] == "L"{
+        // Metodo para instuccion
+        })
 
 
+        else if (instructions[i][0] == "F"{
+        // Metodo para instuccion
+        })
 
-for (int i=0; i < instructions.size();i++){
-    for (int j=0; j <instructions[i].size();j++){
-        cout << instructions[i][j] <<" ";
-    }
-    cout << "\n";
+        else if (instructions[i][0] == "E"{
+        // Metodo para instuccion
+        })
+
+        else if (instructions[i][0] == "C"{
+        // No hacer nada
+        })
+
+        else {
+
+            cout << "Error" << endl;
+        }
 }
-
-
-
-
-
 
     return 0;
 }
