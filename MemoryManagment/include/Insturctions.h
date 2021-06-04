e23267f191b5edcf3336619d39ddbe4f791389db
Roberto <roberto.cantu@gmail.com>
2021-06-03 23:09:50 -0500

    Add some variables

--- MemoryManagment/include/Insturctions.h
+++ MemoryManagment/include/Insturctions.h
@@ -1,5 +1,6 @@
 #ifndef INSTURCTIONS_H
 #define INSTURCTIONS_H
+#include <math.h>
 
 using namespace std;
 
@@ -17,6 +18,14 @@ class Insturctions
 
 
     private:
+        int current_time=0;
+        int page_faults=0;
+        int total_swaps=0;
+        const int MEM_SIZE= 2048;
+        const int SWAP_MEM_SIZE=4096;
+        const int PAGE_SIZE=16;
+        bool strategy= false;
+
 };
 
 void Insturctions::instA(int d , int p, int m){
@@ -25,6 +34,25 @@ void Insturctions::instA(int d , int p, int m){
 }
 
 void Insturctions::instP(int n, int p ){
+    cout << "Asignar " << n << "bytes al proceso " << p << endl;
+    //Casos invalidos
+    if (n <=0){
+        cout << "Error: el tamano del proceso debe ser mayor que cero " << "\n";
+        cout << "Esta instruccion no podra ejecturase " << endl;
+    }
+
+    if (n > 2048){
+        cout << "Error : el tamano del proceso excede los 2048 bytes" << "\n";
+        cout << "Esta instruccion no podra ejecutarse" << endl;
+    }
+
+    if (p < 0){
+        cout << "Error : los id's tienes que ser igual o mayor que cero" << "\n";
+    }
+
+//Calcula cuantas paginas se necesitan para cargar el proceso
+int num_of_pages= ceil(n/PAGE_SIZE);
+
 
 
 }
