fdfd2346d69abdc5b646dde89060ac11a2328ff4
Hugo <A01177091@itesm.mx>
2021-06-07 00:08:37 -0500

    Ya esta todo

--- MemoryManagment/Insturctions.h
+++ MemoryManagment/Insturctions.h
@@ -10,7 +10,7 @@ class Insturctions
         Insturctions();
         //virtual ~Insturctions();
         void instA (int d, int p, int m );
-        void instP (int n, int p);
+        void instP (int n, int p, bool f);
         void instL (int p);
         void instF ();
 
@@ -18,22 +18,29 @@ class Insturctions
 
     private:
 
-        unordered_map <int,int> fifo;
-        unordered_map <int, int> timestamp;
-        int swa[4096] = {0};
-        int mp [127]= {0};
-        int m [2048]= {0};
-        int page_faults=0;
-        int time_stamp=0;
-        int libre=128;
-        int swaps=0;
-        int num_pages=0;
+        unordered_map <int,int> fifo; //mapa para guardar marco de pagina y id
+        unordered_map<int, pair<int, int> > lru; //mapa para guardar tiempo, id y marco de pagina
+        unordered_map <int, int> timestamp; //mapa para guardar tiempo del proceso
+        int swa[256] = {0}; //Variable para representar memoria de swapping
+        int mp [127]= {0}; //Variable para representar marcos de pagina
+        int m [2048]= {0}; ///Variable para representar bytes de memoria real
+        int page_faults=0; //Variable para contar los page faults
+        int time_stamp=0; //Variable para contar el tiempo
+        int libre=128; //Variable para representar las paginas de la memoria
+        int swaps=0; //Variable para contar los page swaps
+        int num_pages=0; //Variable para contar numeros de paginas de cada proceso
         int c = 0;
+        int countA = 0;
         void FIFO(int n, int p);
-
+        void LRU(int n, int p);
+        void framesReal(int p, bool flag);
+        void framesSwa(int p);
+        void erase(int p);
+        bool existeProceso(int p);
 
 
 };
+
 
 
 //Default Constructor
@@ -41,38 +48,69 @@ Insturctions::Insturctions(){
 
 }
 
-void Insturctions::FIFO(int numB, int proc){
+//Metodo con el algoritmo FIFO recibiendo como parametro numero de bytes (N) y id del proceso (pId)
+void Insturctions::FIFO(int N, int pId){
   int index=0;
   int temp3=0;
 
-  c = ceil(numB/16.0);
+  c = ceil(N/16.0);
   //libre -= c;
 
   for(int i=0;i<c;i++)
   {
     //Trae la última posición del mapa
-    unordered_map<int,int>::iterator lastElement;
+    unordered_map<int,int>::iterator iter;
     for(auto it = fifo.begin(); it != fifo.end(); ++it)
     {
-      lastElement = it;
+      iter = it;
     }
 
     //Swap
-    swa[i]=proc;
-    index = lastElement->first;
+    swa[i]=pId;
+    index = iter->first;
     temp3 = mp[index];
     mp[index]=swa[i];
     swa[i]=temp3;
     fifo.erase(index);
-    fifo.insert(make_pair(index,proc));
-    swaps+=2;
+    fifo.insert(make_pair(index,pId));
+    swaps++;
   }
+}
+
+//Metodo con el algortimo LRU recibiendo como parametros numero de bytes (N) y id del proceso (pId)
+void Insturctions::LRU(int N, int pId){
+
+  c = ceil(N/16.0);
+  int intMax = 2147483647;
+    //Trae la última posición del mapa
+    unordered_map<int,pair<int, int> >::iterator iter;
+    for(auto it = lru.begin(); it != lru.end(); ++it)
+    {
+      if(it->second.second < intMax)
+      {
+        iter = it;
+        intMax = it->second.second;
+      }
+    }
+   
+    //Swap
+
+    swa[iter->first] = iter->second.first;
+    mp[iter->first] = pId;
+    //cout<<iter->first;
+    lru.erase(iter->first);
+    lru.insert(make_pair(iter->first, make_pair(pId, intMax)));
+    swaps++;
 }
 
 //Funcion para mostrar la direccion real, dada la direccion virtual
 
 void Insturctions::instA(int d , int p, int m){
-    cout<<"A" << d << " " << p << " " << m << endl;
+    cout<<"A " << d << " " << p << " " << m << endl;
+    if(!existeProceso(p)){
+        cout<<"Ese proceso no existe "<<endl;
+        return;
+    }
     if(m == 0){
         cout << "Obtener la direccion real correspondiente a la direccion virtual " << d << " del proceso " << p << endl;
     }
@@ -99,25 +137,30 @@ void Insturctions::instA(int d , int p, int m){
 
     cout << "Direccion virtual: " << d << " Direccion real: " << res << endl;
     int num = timestamp[p];
-    timestamp[p] = num+1;
+    timestamp[p] = num + 1;
+    countA++;
 }
 
-//Funcion para colocar un proceso
-void Insturctions::instP(int N, int p ){
-    cout << "Asignar " << N << "bytes al proceso " << p << endl;
+//Funcion para colocar un proceso 
+void Insturctions::instP(int N, int p, bool flag){
+
+    cout << "Asignar " << N << " bytes al proceso " << p << endl;
     //Casos invalidos
     if (N <=0){
         cout << "Error: el tamano del proceso debe ser mayor que cero " << "\n";
         cout << "Esta instruccion no podra ejecturase " << endl;
+        return;
     }
 
     if (N > 2048){
         cout << "Error : el tamano del proceso excede los 2048 bytes" << "\n";
         cout << "Esta instruccion no podra ejecutarse" << endl;
+        return;
     }
 
     if (p < 0){
         cout << "Error : los id's tienes que ser igual o mayor que cero" << "\n";
+        return;
     }
 
     num_pages= ceil(N/16.0);
@@ -133,6 +176,7 @@ void Insturctions::instP(int N, int p ){
             if (mp[i] == 0 && num_pages > 0){
                 mp[i]= p;
                 fifo.insert(make_pair(i,p));
+                lru.insert(make_pair(i,make_pair(p, num_pages)));
                 num_pages--;
 
             }
@@ -145,31 +189,7 @@ void Insturctions::instP(int N, int p ){
             }
         }
 
-         int temp = -1, mayor = -1;
-    int iC = 0;
-    int iC2 = 0;
-    int j = 0;
-    for(j=0; j<128; j++)
-    {
-      if(mp[j] == p)
-      {
-        if(iC==0)
-        {
-          temp = j; //temp == 127 iC == 128
-          iC++;
-          cout<<"hola";
-        }
-        if(mp[j+1] != p)
-        {
-          if(iC2==0)
-          {
-            mayor = j;
-            iC2++;
-          }
-        }
-      }
-    }
-    cout << "Se asignaron los marcos de página " << temp << " - " << mayor << " al proceso " << p << endl;
+         framesReal(p, true);
   }
 
 
@@ -177,8 +197,13 @@ void Insturctions::instP(int N, int p ){
 
 
         else {
-            cout << "fifo"<<endl;
-            FIFO(N, p);
+            if(flag){
+                cout << "FIFO"<<endl;
+                FIFO(N, p);
+            }else{
+                cout<<"LRU"<<endl;
+                LRU(N, p);
+            }
         }
 }
 
@@ -187,12 +212,124 @@ void Insturctions::instP(int N, int p ){
 
 
 
-
+//Funcion para liberar proceso
 void Insturctions::instL(int p){
+    cout << "L " << p << endl;
+    if(!existeProceso(p)){
+        cout<<"Ese proceso no existe"<<endl;
+        return;
+    }
+    cout << "Liberar los marcos de página ocupados por el proceso " << p << endl;
+    int time = timestamp[p];
+    timestamp[p]=time+1;
+
+    framesReal(p, false);
+    erase(p);
+    framesSwa(p);
+}
+
+
+//Funcion para imprimir reporte de resultados
+void Insturctions::instF(){
+    int contador = 0;
+    int auxiliar = 0;
+    cout << "Turnaround por proceso: " << endl;
+    for(auto it = timestamp.begin(); it != timestamp.end(); ++it)
+    {
+        cout << "Nombre del proceso: " << it->first << " Turnaround: " << it->second << endl;
+        contador++;
+        auxiliar+=it->second;
+    }
+    cout << "Turnaround promedio: " << (auxiliar/(contador*1.0))-4 << endl;
+    cout << "Swaps general: " <<  swaps << endl;
+    cout << "Page faults: " << '1' << endl;
+    cout << "Tiempo de acceso o modificacion de direccion " << countA * 0.1 << 's' << endl;
 
 }
 
-void Insturctions::instF(){
+//funcion auxiliar para obtener marcos paginas 
+void Insturctions::framesReal(int p, bool flag){
+      int aux = -1;
+      int last = -1;
+      int pages = 0;
+      int auxPage = 0;
+      for(int j = 0; j<128; j++){
+        if(mp[j] == p){
+        //cout<<"hola";
+          if(pages == 0){
+            aux = j;
+            pages++;
+          }
+          if(mp[j+1] != p){
+            if(auxPage == 0){
+              last = j;
+              auxPage++;
+            }
+          }
+        }
+      }
+      if(flag){
+        cout << "Se asignaron los marcos de pagina " << aux << " - " << last << " al proceso " << p << endl;
+      }else{
+        cout << "Se liberan los marcos de página de memoria real: " << aux << " - " << last << endl;
+      }
+    }
 
 
+//funcion auxiliar para obtener marcos paginas de la memoria swap
+void Insturctions::framesSwa(int p){
+      int aux = -1;
+      int last = -1;
+      int pages = 0;
+      int auxPage = 0;
+      for(int j = 0; j<256; j++){
+        if(swa[j] == p){
+        //cout<<"hola";
+          if(pages == 0){
+            aux = j;
+            pages++;
+          }
+          if(swa[j+1] != p){
+            if(auxPage == 0){
+              last = j;
+              auxPage++;
+            }
+          }
+        }
+      }
+        if(aux != -1){
+            cout << "Se liberan los marcos " << aux << " - " << last << " del área de swapping" << endl;
+        }
+
+         for(int i=0;i<256;i++){
+            if(swa[i]==p){
+                swa[i]=0;
+            }
+        }
+
+    }
+
+//Funcion para borrar marcos de pagina de un proceso
+void Insturctions::erase(int p){
+    for(int i=0;i<127;i++)
+  {
+    if(mp[i]==p)
+    {
+      libre++;
+      mp[i]=0;
+      fifo.erase(i);
+    }
+  }
+}
+
+//Funcion que revisa si existe un proceso en memoria real
+bool Insturctions::existeProceso(int p){
+    for(int i = 0, cont = 0; i<127; i++){
+        if(mp[i] != p){
+            cont++;
+        }else{
+            return true;
+        }
+    }
+    return false;
 }
--- MemoryManagment/main.cpp
+++ MemoryManagment/main.cpp
@@ -9,6 +9,8 @@
 
 
 using namespace std;
+
+//Objeto con clase de instrucciones con los metodos A,P,L,F,E, Y C
 Insturctions instructionsObj;
 //Funcion que convierte de String a Int
 int strtoInt (string instruction){
@@ -21,15 +23,19 @@ int strtoInt (string instruction){
 
 int main()
 {
+    bool lru;
+    cout<<"Ponga 1 para elegir FIFO, ponga 0 para elegir LRU "<<endl;
 
-
+    cin>>lru;
 
 
 vector <vector <string>> instructions;
 
+// Inicializa archivo de texto
+Parser parser ("ArchivoTrabajo.txt");
 
-Parser parser ("test.txt");
 
+//Vector con instrucciones
 instructions = parser.getParseInstructions();
 
 
@@ -39,55 +45,55 @@ instructions = parser.getParseInstructions();
 
 /*
 Opciones posibles
-            A - 3 args, all numbers
-            P - 2 args, all numbers
-            L - 1 arg, number
-            F - 0 args
-            E - 0 args
-            C - all comments
+            A - 3 args, all numbers  ---- Lee y modifica direcciones de procesos
+            P - 2 args, all numbers ---- Asigna procesos a memoria real
+            L - 1 arg, number ---- Libera procesos
+            F - 0 args ---- Reporte de resultados
+            E - 0 args ---- Termina ejecucion
+            C - all comments ---- Agrega comentarios
             */
 
-/*for(int i = 0; i<instructions.size(); i++){
-    for(int j = 0; j<instructions[i].size(); j++){
-        cout<<instructions[i][j];
-    }
-}
-*/
+
 for (int i=0; i < instructions.size(); i++){
 
-    if (instructions[i][0] == "A"){
-
-        // functionA (strtoInt (instuctions [i][1], instuctions [i][2], instuctions [i][3])
+    if (instructions[i][0] == "A"){ 
 
         instructionsObj.instA(strtoInt(instructions[i][1]), strtoInt(instructions[i][2]), strtoInt(instructions[i][3]));
         }
 
         else if (instructions[i][0] == "P"){
 
-        instructionsObj.instP(strtoInt(instructions[i][1]), strtoInt(instructions[i][2]));
+        instructionsObj.instP(strtoInt(instructions[i][1]), strtoInt(instructions[i][2]), lru);
         // Metodo para instuccion
         }
 
         else if (instructions[i][0] == "L"){
+        instructionsObj.instL(strtoInt(instructions[i][1]));
         // Metodo para instuccion
         }
 
 
         else if (instructions[i][0] == "F"){
+            instructionsObj.instF();
         // Metodo para instuccion
         }
 
         else if (instructions[i][0] == "E"){
-        // Metodo para instuccion
+            cout<<"Se termino el programa, ponga un 100 profe"<<endl;
+            return 0;
         }
 
         else if (instructions[i][0] == "C"){
-        // No hacer nada
+            for(int j = 0; j<instructions[i].size(); j++){
+                 cout<<instructions[i][j]<<" ";
+            }
+            cout<<endl;
         }
 
         else {
 
             cout << "Error" << endl;
+            return 0;
         }
 }
 
